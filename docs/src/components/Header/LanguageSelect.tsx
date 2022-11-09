0766b75d2fb5d22e43afeaabe1925f162652b6f3
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2022-11-09 23:41:01 +0900

    Modify index

--- docs/src/components/Header/LanguageSelect.tsx
+++ docs/src/components/Header/LanguageSelect.tsx
@@ -31,7 +31,7 @@ const LanguageSelect: FunctionComponent<{ lang: string }> = ({ lang }) => {
 					const newLang = e.target.value;
 					let actualDest = window.location.pathname.replace(langPathRegex, '/');
 					if (actualDest == '/') actualDest = `/guide`;
-					window.location.pathname = '/' + newLang + actualDest;
+					window.location.pathname = '/json-struct/' + newLang + actualDest;
 				}}
 			>
 				{Object.entries(KNOWN_LANGUAGES).map(([key, value]) => {
